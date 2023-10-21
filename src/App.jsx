02f885d2c71105a290db22001450f9fe32d01e43
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-10-21 20:55:42 +0300

    Final project with currency converter and Loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState('EUR');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [converted, setConverted] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  // run this on component mount
+  useEffect(function() {
+    async function convert() {
+      setIsLoading(true);
+      const response = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`);
+      const data = await response.json();
+      setConverted(data.rates[toCurrency]);
+      setIsLoading(false);
+    }
+
+    // we don;t want to convert if same currencies are entered
+    if(fromCurrency === toCurrency) return setConverted(amount);
+    convert();
+  }, [amount, fromCurrency, toCurrency]); // any of these 3 changes, we should remount 
+
   return (
-    <>
-      
-    </>
+    <div>
+      <input 
+        type="text" 
+        value={amount} 
+        onChange={(e) => setAmount(Number(e.target.value))} 
+        disabled={isLoading}
+      />
+        <select value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)} disabled={isLoading}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)} disabled={isLoading}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+      <p>{converted} {toCurrency}</p>
+    </div>
   )
 }
 
